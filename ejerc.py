3d5c650901f293b1ef4640b6d7a6a88bcafd8895
Raquelolimar <raquelolivenza@gmail.com>
2025-03-12 20:13:29 +0100

    miercoles

---
+++ ejerc.py
@@ -0,0 +1,84 @@
+1
+sentence = "this dog is white"
+number = 12
+my_list = [ 1, 2, 3]
+boleano = 5<6
+
+print(sentence,number, my_list, boleano)
+
+2
+starter_sentence= "this dog is white"
+
+print(starter_sentence[:3])
+
+3
+my_list = [ 1, 2, 3]
+print( my_list [0])
+
+4
+number = 12
+number_two = 10
+
+print(number + number_two)
+
+5
+
+my_list =[ 1, 2, 3]
+print(my_list [-1])
+
+6
+names= "harry, alex, susie, jared, gail, conner"
+list_names = names.split(",")
+
+print(list_names)
+
+7
+sentence = "this dog is white" 
+print(sentence[:4].upper())
+
+
+sentence = "this dog is white"
+sentence_two= sentence[:4].upper()
+
+print(sentence_two, sentence)
+
+
+8
+number = 12
+total = f'gente para concursar{number}'
+print(total)
+
+
+
+
+9
+
+text = "hello world"
+print(text)
+
+10
+
+sentence = "hola me llamo Ibai"
+sentence = sentence.replace("hola", "adios")
+print(sentence)
+
+
+
+
+
+
+
+name = "Alba"
+greeting = f'hi {name}'
+print(greeting)
+
+name = "Alba"
+product = "white jeans"
+content = f"""
+hi {name}
+
+thanks for using our {product}
+"""
+print(content)
+
+
